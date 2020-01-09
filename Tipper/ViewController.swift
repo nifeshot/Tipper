cfb9b5f09b8361a7261171b75a3530e97642417f
Adam <billparker149@gmail.com>
2020-01-09 09:47:39 -1000

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Daniel Lei on 1/9/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
